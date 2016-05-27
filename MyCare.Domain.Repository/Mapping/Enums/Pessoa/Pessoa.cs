9ed3cc2fd74cfa167d03aaae15a64e296b8322a4
Ramon Quirino <Ramon Quirino>
2016-05-27 12:05:16 -0300

    first commit

---
+++ MyCare.Domain.Repository/Mapping/Enums/Pessoa/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace MyCare.Domain.Repository.Mapping.Enums.Pessoa
+{
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminino = 2
+    }
+
+    public enum EstadoCivil
+    {
+        Solteiro = 1,
+        Casado = 2,
+        Divorciado = 3,
+        Viuvo = 4
+    }
+}
---
+++ MyCare.Domain.Repository/Mapping/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using MyCare.Domain.Repository.Mapping.Enums.Pessoa;
+
+namespace MyCare.Domain.Repository.Mapping
+{
+    public class Pessoa
+    {
+        public long ID { get; set; }
+        public string Nome { get; set; }
+        public string Sobreno { get; set; }
+        public DateTime Nascimento { get; set; }
+        public Sexo Sexo { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+    }
+
+   
+}
